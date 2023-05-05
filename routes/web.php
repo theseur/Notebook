95a4ddc8e12c8d0a5033f90b8db3bf6e714fc4a7
Farkas Tibor <32579497+theseur@users.noreply.github.com>
2023-05-05 21:02:37 +0200

    1st table

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+Route::get('/register', [RegisteredUserController::class, 'create'])
+->middleware('guest')
+->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store'])
+->middleware('guest');
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+->middleware('guest')
+->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+->middleware('guest');
